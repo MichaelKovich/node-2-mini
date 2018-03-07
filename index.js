e50f452fae06dca3cddf240c891c4f7344cfb43c
Michael Kovich <kovich@michaelkovich.com>
2018-03-07 17:01:48 -0600

    Project complete.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+const controller = require('./controller');
+require('dotenv').config();
 
+// Instantiate Express
 const app = express();
-app.use( bodyParser.json() );
-app.use( cors() );
 
-const port = process.env.PORT || 3000
-app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
+// Invoke massive, pass in CONNECTION_STRING from .env, and then
+massive(process.env.CONNECTION_STRING).then((dbInstance) => {
+  app.set('db', dbInstance); // Give our Express application access to the database.
+  // dbInstance.new_planes() // Call dbInstance.new.planes
+  //   .then(planes => console.log(planes)) // .then console.log(planes)
+  //   .catch(err => console.log(err)); // Catch and console.log errors
+  // dbInstance.get_planes()
+  //   .then(planes => console.log(planes))
+  //   .catch(err => console.log(err));
+});
 
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/api/planes', controller.getPlanes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
